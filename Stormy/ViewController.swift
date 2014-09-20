1f79a7530b678ff0adfffdb311181465b0f1e6da
Matheus Bras <bras.matheus@gmail.com>
2014-09-19 21:18:45 -0300

    Add refresh and network error handling

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -17,10 +17,17 @@ class ViewController: UIViewController {
     @IBOutlet var humidity: UILabel!
     @IBOutlet var precipitation: UILabel!
     @IBOutlet var summary: UILabel!
+    @IBOutlet var refreshButton: UIButton!
+    @IBOutlet var refreshActivityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
+    }
+    
+    func getCurrentWeatherData() -> Void {
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
         let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
         
@@ -30,7 +37,6 @@ class ViewController: UIViewController {
             if (error == nil) {
                 let dataObject = NSData(contentsOfURL: location)
                 let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject, options: nil, error: nil) as NSDictionary
-
                 
                 let currentWeather = Current(weatherDictionary: weatherDictionary)
                 
@@ -41,14 +47,42 @@ class ViewController: UIViewController {
                     self.humidity.text = NSString(format: "%.2f", currentWeather.humidity)
                     self.precipitation.text = NSString(format: "%.2f", currentWeather.precipProbability)
                     self.summary.text = currentWeather.summary
+                    
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshButton.hidden = false
+                    self.refreshActivityIndicator.hidden = true
+                })
+            } else {
+                let networkIssueController = UIAlertController(title: "Error", message: "Unable to load data. Connectivity error!", preferredStyle: .Alert)
+ 
+                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                networkIssueController.addAction(okButton)
+                
+                let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                networkIssueController.addAction(cancelButton)
+                
+                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshButton.hidden = false
+                    self.refreshActivityIndicator.hidden = true
                 })
             }
             
         })
         
         downloadTask.resume()
-        
     }
+    
+    @IBAction func refresh() {
+        getCurrentWeatherData()
+        
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
